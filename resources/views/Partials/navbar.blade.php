4607134a932754436758e7cc8034b46c436264ce
192845-AlexisO <al192845@alumnos.uacj.mx>
2023-07-19 19:41:50 -0600

    Update styles

--- resources/views/Partials/navbar.blade.php
+++ resources/views/Partials/navbar.blade.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="{{asset('storage\css\app.css') }}">
-    <link rel="icon" href="{{ asset('storage\logos\logo5.png') }}" type="image/png">
+    <link rel="stylesheet" href="{{asset('css\app.css') }}">
+    <link rel="icon" href="{{ asset('img\logo.png') }}" type="image/png">
     <title>Aries</title>
 </head>
 <body class="background-body" >
@@ -15,7 +15,7 @@
     <header>
         <nav class="navbar sticky-top background-nav" >
             <div class="container-fluid align-items-center">
-                <a class="navbar-brand " href="{{URL::to('/')}}" style="width:100px;height:100px;border-radius: 50%"><img src="{{asset('storage\logos\logo5.png') }}" class="" alt="Logo" style="width:100px;height:100px"></a>
+                <a class="navbar-brand " href="{{URL::to('/')}}" style="width:100px;height:100px;border-radius: 50%"><img src="{{asset('img\logo.png') }}" class="" alt="Logo" style="width:100px;height:100px"></a>
                     <button class="navbar-toggler background-button-nav" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
